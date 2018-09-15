55d5236e66a223d48094b1a64b02dc6a46eceff2
Alan Xu <ayx002@ucsd.edu>
2018-09-14 17:48:13 -0700

    adds marvel table component

---
+++ src/components/MarvelTable.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="marvel-table">
+    Marvel Table
+  </div>
+</template>
+
+<script>
+export default {
+  name: "marvel-table",
+};
+</script>
+
+<style lang="scss">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import MarvelTable from './components/MarvelTable.vue'
 
 new Vue({
   el: '#app',
   render: h => h(App)
+})
+
+new Vue({
+  el: '#marvel-table',
+  render: h => h(MarvelTable)
 })
